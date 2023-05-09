6c8c4a46b12c47a06785518b66a45b78d44e2c15
Jan Kowalski <jan@kowalski.com>
2023-05-09 13:29:19 +0200

    added javascript and crap

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,25 @@
 "use strict";
+
+function calc() {
+    const buttonValue = this.innerText;
+    //this.innerText will work instead of target because it's a normal arrow function
+    const resPlace = document.querySelector('#res');
+
+    if(buttonValue === "C") {
+        resPlace.value = '';
+    } else if(buttonValue === "=") {
+        resPlace.value = eval(resPlace.value);
+        //eval takes the text inside the input and automatically calculates it like 5 * 7 or 2 - 6 + 7
+    } else {
+        resPlace.value += buttonValue;
+        //adds the text that's inside the button tag into the input field
+    }
+}
+
+const buttons = document.querySelectorAll("button");
+//create a collection called buttons
+
+buttons.forEach(element => {
+    element.addEventListener('click', calc);
+    //callback to function
+})
